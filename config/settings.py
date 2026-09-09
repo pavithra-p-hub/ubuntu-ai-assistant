fbed9a1d7712526dae3de76944296c7085ea470b
Pavithra <paviselvam02@gmail.com>
2026-09-09 15:17:29 +0530

    Move Django secret key to environment variable

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ ANTHROPIC_API_KEY=os.getenv("ANTHROPIC_API_KEY")
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_we7yr)*+^7pw%il_*dq18j4(mdi2f+6u9)68%y=+ck)@vsnjd'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
